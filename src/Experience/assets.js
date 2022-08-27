c131085b841aa1f40a217f3def5417bb8704f844
GhostCat <gcatcg@gmail.com>
2022-08-27 16:17:04 +0800

    fix: asstes add dot

--- src/Experience/assets.js
+++ src/Experience/assets.js
@@ -4,10 +4,10 @@ export default [
         data: {},
         items:
         [
-            { name: 'bakedDayTexture', source: '/assets/bakedDay.jpg', type: 'texture' },
-            { name: 'bakedNightTexture', source: '/assets/bakedNight.jpg', type: 'texture' },
-            { name: 'lightMapTexture', source: '/assets/lightMap.jpg', type: 'texture' },
-            { name: 'roomModel', source: '/assets/city.glb' },
+            { name: 'bakedDayTexture', source: './assets/bakedDay.jpg', type: 'texture' },
+            { name: 'bakedNightTexture', source: './assets/bakedNight.jpg', type: 'texture' },
+            { name: 'lightMapTexture', source: './assets/lightMap.jpg', type: 'texture' },
+            { name: 'roomModel', source: './assets/city.glb' },
         ]
     }
 ]
